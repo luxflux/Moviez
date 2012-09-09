c3f823f8aeefcf5f85873330c0db35598f6c3984
Raffael Schmid <raffael@yux.ch>
2012-09-09 21:46:59 +0200

    add a human readable format of the watched attribute

--- app/assets/javascripts/moviez/models/movie.js
+++ app/assets/javascripts/moviez/models/movie.js
@@ -2,5 +2,14 @@ Moviez.Movie = DS.Model.extend({
   title: DS.attr('string'),
   description: DS.attr('string'),
   discNumber: DS.attr('number'),
-  watched: DS.attr('boolean')
+  watched: DS.attr('boolean'),
+
+  watched_human: function() {
+    if(this.get('watched')) {
+      return 'Watched';
+    } else {
+      return 'Not yet watched';
+    }
+  }.property('watched')
+
 });
