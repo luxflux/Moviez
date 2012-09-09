aa0fd662c4068ffde4b69b319de472a4dba43843
Raffael Schmid <raffael@yux.ch>
2012-09-09 13:35:57 +0200

    setup the router

--- app/assets/javascripts/moviez/routes/app_router.js
+++ app/assets/javascripts/moviez/routes/app_router.js
@@ -3,14 +3,20 @@ Moviez.Router = Ember.Router.extend({
 
   root: Ember.Route.extend({
     index: Ember.Route.extend({
-      route: '/'
-
-      // You'll likely want to connect a view here.
-      // connectOutlets: function(router) {
-      //   router.get('applicationController').connectOutlet(App.MainView);
-      // }
-
-      // Layout your routes here...
+      route: '/',
+      redirectsTo: 'movies'
+    }),
+    movies: Ember.Route.extend({
+      route: '/movies',
+      connectOutlets: function(router) {
+        router.get('applicationController').connectOutlet('movies', Moviez.Movie.find());
+      }
+    }),
+    movie: Ember.Route.extend({
+      route: '/movie/:movie_id',
+      connectOutlets: function(router, movie) {
+        router.get('applicationController').connectOutlet('movie', movie);
+      }
     })
   })
 });
