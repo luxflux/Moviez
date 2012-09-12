95a7b5e3a980da056ca92d1434533101c2ce3500
Raffael Schmid <raffael@yux.ch>
2012-09-13 00:22:57 +0200

    add some serverside validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,8 @@
 class Movie < ActiveRecord::Base
   attr_accessible :description, :disc_number, :title, :watched
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :disc_number, presence: true
+
 end
