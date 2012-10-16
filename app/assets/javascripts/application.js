0655ad25792978cb0facb9ef83f5883b09c2ed88
Raffael Schmid <raffael@yux.ch>
2012-10-16 21:00:17 +0200

    add emberjs stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,10 @@
 //= require jquery_ujs
 //= require foundation
 //
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require moviez/moviez
+Moviez = Ember.Application.create();
 //= require_tree .
