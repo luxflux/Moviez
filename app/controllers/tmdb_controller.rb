068ab123051090c934adfd7bb03bd437d0b0f2db
Raffael Schmid <raffael@yux.ch>
2012-10-17 13:21:56 +0200

    Revert "generate tmdb controller"
    This reverts commit 019934ac151a2f3d148369b785796d5e0092b487.

--- app/controllers/tmdb_controller.rb
+++
@@ -1,4 +0,0 @@
-class TmdbController < ApplicationController
-  def search
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Moviez::Application.routes.draw do
-  get "tmdb/search"
-
   resources :movies
 
   root :to => 'movies#index'
