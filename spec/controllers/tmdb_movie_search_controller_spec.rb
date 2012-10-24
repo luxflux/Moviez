cc41d99626a850bbd4809b5b4c4f03154e2998ba
Raffael Schmid <raffael@yux.ch>
2012-10-24 14:38:50 +0200

    some tests for the tmdb_movie_search_controller

--- spec/controllers/tmdb_movie_search_controller_spec.rb
+++ spec/controllers/tmdb_movie_search_controller_spec.rb
@@ -1,10 +1,19 @@
 require 'spec_helper'
+require 'ostruct'
 
 describe TMDBMovieSearchController do
 
+  let(:data_from_tmdb) do
+    OpenStruct.new(
+      id: 1,
+      imdb_id: 't123',
+    )
+  end
+
   describe "GET 'show'" do
     it "returns http success" do
-      get 'show'
+      TMDB::Movie.should_receive(:find_by_id).with("1").returns(data_from_tmdb)
+      get 'show', id: 1
       response.should be_success
     end
   end
