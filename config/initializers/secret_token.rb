54d98ed8905b76c47ff871ff10a15aff2748540f
Raffael Schmid <raffael@yux.ch>
2012-12-31 14:29:17 +0100

    secure the secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Moviez::Application.config.secret_token = '9a5d8fd48d8e50734211ca4485359e681c1e1b7a73d7dca08a7e5cc9d1f7275214f75347e996811464f1cc4d08e902fc0ea5dd6a706e7bbcb39124efa8d2fdf0'
+Moviez::Application.config.secret_token = ENV['SECRET_TOKEN']
