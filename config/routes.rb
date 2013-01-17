3199101218afcb632be84e2db363f49ab18fe4b8
Raffael Schmid <raffael@yux.ch>
2013-01-17 09:09:53 +0100

    fix paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Moviez::Application.routes.draw do
 
-  resources :tmdb_movie, only: [:index, :show]
+  resources :tmdb_movies, only: [:index, :show]
 
   resources :movies do
     put :auto_update, on: :member
