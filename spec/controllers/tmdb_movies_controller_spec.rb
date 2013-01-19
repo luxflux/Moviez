c18611c62b57b08e0a3a2b38a2c8f1a922593177
Raffael Schmid <raffael@yux.ch>
2013-01-19 14:31:06 +0100

    i more like symbols

--- spec/controllers/tmdb_movies_controller_spec.rb
+++ spec/controllers/tmdb_movies_controller_spec.rb
@@ -8,7 +8,7 @@ describe TMDBMoviesController do
   describe "GET 'show'" do
     it "returns http success" do
       TMDB::Movie.should_receive(:find_by_id).with("1").and_return(tmdb_movie)
-      get 'show', id: 1
+      get :show, id: 1
       response.should be_success
     end
   end
@@ -16,7 +16,7 @@ describe TMDBMoviesController do
   describe "GET 'index'" do
     it "returns http success" do
       TMDB::Movie.should_receive(:search).with(title: 'Star Wars').and_return([tmdb_movie, tmdb_movie])
-      get 'index', title: 'Star Wars'
+      get :index, title: 'Star Wars'
       response.should be_success
     end
   end
