baa5acd8439965caf130322e06fb8fac89de0839
Raffael Schmid <raffael@yux.ch>
2013-01-19 14:28:32 +0100

    fix form and add test for it

--- spec/views/movies/new.html.haml_spec.rb
+++ spec/views/movies/new.html.haml_spec.rb
@@ -2,11 +2,15 @@ require 'spec_helper'
 
 describe "movies/new.html.haml" do
   before do
+    controller.stub(:controller_name).and_return('movies')
     assign(:movie, stub_model(Movie))
   end
 
-  it 'includes the tmdb_movie form' do
-    render
-  end
+  before { render }
+  subject { rendered }
+
+  it { should have_css("form > input#title") }
+  it { should have_css("script#movie-preview-title-template") }
+  it { should have_css("script#movie-preview-template") }
 end
 
