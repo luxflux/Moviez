9f33d042ad37a286449bcd9f3de6d742e8aba7d3
Raffael Schmid <raffael@yux.ch>
2013-01-20 16:46:45 +0100

    enable caching always, except in testing

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,7 @@ module Moviez
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.cache_store = :mem_cache_store, 'localhost'
   end
 end
