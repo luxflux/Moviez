d425f387dfbb1127c73ce058b33f54e38b8f5716
Raffael Schmid <raffael@yux.ch>
2013-02-12 14:26:34 +0100

    test handling of unauthorized requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from(CanCan::AccessDenied) do
+    flash[:error] = I18n.t('cancan.access_denied')
+    redirect_to :root
+  end
 end
--- spec/integration/add_movie_spec.rb
+++ spec/integration/add_movie_spec.rb
@@ -4,29 +4,37 @@ describe 'add a movie' do
 
   let(:tmdb_movie) { tmdb_result }
 
-  it 'searches for the given title', js: true do
-    user = FactoryGirl.create(:user, password: 'testing123')
-    visit new_user_session_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: 'testing123'
-    click_on 'Sign in'
+  context 'as an authorized user' do
+    it 'searches for the given title', js: true do
+      user = FactoryGirl.create(:user, password: 'testing123')
+      visit new_user_session_path
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: 'testing123'
+      click_on 'Sign in'
 
-    TMDB::Movie.should_receive(:search).with(title: 'Star Wars').and_return([tmdb_movie])
-    TMDB::Movie.should_receive(:find_by_id).with("1").twice.and_return(tmdb_movie)
+      TMDB::Movie.should_receive(:search).with(title: 'Star Wars').and_return([tmdb_movie])
+      TMDB::Movie.should_receive(:find_by_id).with("1").twice.and_return(tmdb_movie)
 
-    visit new_movie_path
-    page.should_not have_content('Star Wars Episode VII')
-    fill_in 'title', with: 'Star Wars'
-    page.should have_content('Star Wars Episode VII')
+      visit new_movie_path
+      page.should_not have_content('Star Wars Episode VII')
+      fill_in 'title', with: 'Star Wars'
+      page.should have_content('Star Wars Episode VII')
 
-    page.should_not have_content('A lot more words!')
-    click_on 'Star Wars Episode VII (1988)'
-    page.should have_content('A lot more words!')
-    click_on 'Select'
+      page.should_not have_content('A lot more words!')
+      click_on 'Star Wars Episode VII (1988)'
+      page.should have_content('A lot more words!')
+      click_on 'Select'
 
-    page.should have_css('h1', text: tmdb_movie.title)
-    current_path.should eq("/movies/#{Movie.last.id}")
-
+      page.should have_css('h1', text: tmdb_movie.title)
+      current_path.should eq("/movies/#{Movie.last.id}")
+    end
   end
 
+  context 'as an unauthorized user' do
+    it 'does not allow access' do
+      visit new_movie_path
+      current_path.should eq('/')
+      page.should have_content('not authorized')
+    end
+  end
 end
