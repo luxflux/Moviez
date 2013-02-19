987d8eecc5443902cabe27a9b79961c50222671d
Raffael Schmid <raffael@yux.ch>
2013-02-19 21:17:28 +0100

    add a relation from the borrowers to the users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130219081535) do
+ActiveRecord::Schema.define(:version => 20130219201639) do
 
   create_table "borrowers", :force => true do |t|
     t.string   "first_name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130219081535) do
     t.string   "email"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "user_id"
   end
 
   create_table "loans", :force => true do |t|
