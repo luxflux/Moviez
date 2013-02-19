493673039da5cb1417f1923fb65a755223e89b84
Raffael Schmid <raffael@yux.ch>
2013-02-19 21:04:13 +0100

    add validations

--- app/models/borrower.rb
+++ app/models/borrower.rb
@@ -4,4 +4,8 @@ class Borrower < ActiveRecord::Base
   has_many :movies, through: :loans
 
   attr_accessible :email, :family_name, :first_name
+
+  validates :email, presence: true
+  validates :family_name, presence: true
+  validates :family_name, presence: true
 end
--- app/models/loan.rb
+++ app/models/loan.rb
@@ -2,4 +2,6 @@ class Loan < ActiveRecord::Base
   belongs_to :borrower
   belongs_to :movie
   # attr_accessible :title, :body
+
+  validates_presence_of :borrower, :movie
 end
