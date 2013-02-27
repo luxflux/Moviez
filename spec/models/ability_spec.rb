91a93ad43e43278be54c979e38a625e5053f48c1
Raffael Schmid <raffael@yux.ch>
2013-02-27 09:15:42 +0100

    refactor tests

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -6,11 +6,11 @@ describe Ability do
   let(:movie) { FactoryGirl.create(:movie, user_id: user.id) }
   let(:borrower) { FactoryGirl.create(:borrower, user_id: user.id) }
 
+  subject { ability }
+
   describe 'movie rights' do
     context 'as the user of the movie' do
       let(:ability) { Ability.new(user) }
-
-      subject { ability }
 
       it { should be_able_to(:manage, movie) }
     end
@@ -18,16 +18,12 @@ describe Ability do
     context 'as an other user' do
       let(:ability) { Ability.new(FactoryGirl.create(:user)) }
 
-      subject { ability }
-
       it { should_not be_able_to(:manage, movie) }
       it { should be_able_to(:read, movie)}
     end
 
     context 'as an anonymous user' do
       let(:ability) { Ability.new(nil) }
-
-      subject { ability }
 
       it { should_not be_able_to(:manage, movie) }
       it { should be_able_to(:read, movie)}
@@ -38,20 +34,14 @@ describe Ability do
     context 'as the user of the borrower' do
       let(:ability) { Ability.new(user) }
 
-      subject { ability }
-
       it { should be_able_to(:manage, borrower) }
     end
 
     context 'as an other user' do
       let(:ability) { Ability.new(FactoryGirl.create(:user)) }
 
-      subject { ability }
-
-
       it { should_not be_able_to(:manage, borrower) }
       it { should_not be_able_to(:read, borrower)}
-
     end
   end
 
