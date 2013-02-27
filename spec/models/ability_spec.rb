4271d49aeb676fdfc9fe6d7b17a845dc28eff365
Raffael Schmid <raffael@yux.ch>
2013-02-27 09:14:30 +0100

    add tests for the abilites relating to the borrower

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -4,31 +4,55 @@ describe Ability do
 
   let(:user) { FactoryGirl.create(:user) }
   let(:movie) { FactoryGirl.create(:movie, user_id: user.id) }
+  let(:borrower) { FactoryGirl.create(:borrower, user_id: user.id) }
 
-  context 'as the user of the movie' do
-    let(:ability) { Ability.new(user) }
+  describe 'movie rights' do
+    context 'as the user of the movie' do
+      let(:ability) { Ability.new(user) }
 
-    subject { ability }
+      subject { ability }
 
-    it { should be_able_to(:manage, movie) }
+      it { should be_able_to(:manage, movie) }
+    end
+
+    context 'as an other user' do
+      let(:ability) { Ability.new(FactoryGirl.create(:user)) }
+
+      subject { ability }
+
+      it { should_not be_able_to(:manage, movie) }
+      it { should be_able_to(:read, movie)}
+    end
+
+    context 'as an anonymous user' do
+      let(:ability) { Ability.new(nil) }
+
+      subject { ability }
+
+      it { should_not be_able_to(:manage, movie) }
+      it { should be_able_to(:read, movie)}
+    end
   end
 
-  context 'as an other user' do
-    let(:ability) { Ability.new(FactoryGirl.create(:user)) }
+  describe 'borrower rights' do
+    context 'as the user of the borrower' do
+      let(:ability) { Ability.new(user) }
 
-    subject { ability }
+      subject { ability }
 
-    it { should_not be_able_to(:manage, movie) }
-    it { should be_able_to(:read, movie)}
-  end
+      it { should be_able_to(:manage, borrower) }
+    end
 
-  context 'as an anonymous user' do
-    let(:ability) { Ability.new(nil) }
+    context 'as an other user' do
+      let(:ability) { Ability.new(FactoryGirl.create(:user)) }
 
-    subject { ability }
+      subject { ability }
 
-    it { should_not be_able_to(:manage, movie) }
-    it { should be_able_to(:read, movie)}
+
+      it { should_not be_able_to(:manage, borrower) }
+      it { should_not be_able_to(:read, borrower)}
+
+    end
   end
 
 end
