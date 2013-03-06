2a119f646f4b49ff72640e483b9ae369165d18a6
Raffael Schmid <raffael@yux.ch>
2013-03-06 18:59:28 +0100

    use icons for the navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,16 @@ module ApplicationHelper
     request.env['PATH_INFO'] == path
   end
 
+  def class_for_path(path)
+    if current_path?(path)
+      :active
+    else
+      ''
+    end
+  end
+
+  def navigation_link(path, icon, link_options = {})
+    content_tag(:li, link_to(content_tag(:i, '', class: "icon-#{icon}"), path, link_options), class: class_for_path(path))
+  end
+
 end
