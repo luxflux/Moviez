4a6766a8b306ddf1ce51a4d1c28409295ba853a8
Raffael Schmid <raffael@yux.ch>
2013-03-21 09:11:04 +0100

    visually nicer borrowers list page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,7 +13,11 @@ module ApplicationHelper
   end
 
   def navigation_link(path, icon, link_options = {})
-    content_tag(:li, link_to(content_tag(:i, '', class: "icon-#{icon}"), path, link_options), class: class_for_path(path))
+    content_tag(:li, icon_link(path, icon, link_options), class: class_for_path(path))
+  end
+
+  def icon_link(path, icon, link_options = {})
+    link_to(content_tag(:i, '', class: "icon-#{icon}"), path, link_options)
   end
 
 end
